a72f9aca9c7343fe040149af719da046b3d7461b
amela <amela.rakanovic@gmail.com>
2015-10-13 16:31:47 +0200

    Added dn2.

---
+++ Lesson1/dn2.playground/Contents.swift
@@ -0,0 +1,50 @@
+//: Playground - noun: a place where people can play
+// DN2
+
+// ############################################
+// Fibonaccijevo zaporedje iterativno
+
+func fibonacci_iter_print(n:Int) {
+    print(fibonacci_iteration(n))
+}
+
+func fibonacci_iteration(var n:Int) -> Int {
+    if n == 0 { return 0 }
+    var x = 0, y = 1
+    while n > 1 {
+        let c = x + y
+        x = y
+        y = c
+        n -= 1
+    }
+    //print(y)
+    return y
+}
+
+// Fibonacci iterativno: primeri uporabe
+fibonacci_iter_print(0)
+fibonacci_iter_print(1)
+fibonacci_iter_print(2)
+fibonacci_iter_print(5)
+fibonacci_iter_print(12)
+
+
+// ############################################
+// Fibonaccijevo zaporedje rekurzivno
+
+func fibonacci_rec_print(n:Int) {
+    print(fibonacci_recursion(n))
+}
+
+func fibonacci_recursion(n:Int) -> Int {
+    if n == 0 { return 0 }
+    else if n == 1 || n == 2 { return 1 }
+    else { return fibonacci_recursion(n-1) + fibonacci_recursion(n-2) }
+}
+
+// Fibonacci rekurzivno: primeri uporabe
+fibonacci_rec_print(0)
+fibonacci_rec_print(1)
+fibonacci_rec_print(2)
+fibonacci_rec_print(5)
+fibonacci_rec_print(12)
