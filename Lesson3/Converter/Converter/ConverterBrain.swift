d92c49795e4b420052150874d956ece9bf979b68
amela <amela.rakanovic@gmail.com>
2015-10-20 23:23:31 +0200

    Add Converter.

---
+++ Lesson3/Converter/Converter/ConverterBrain.swift
@@ -0,0 +1,44 @@
+/*
+Napiši razred, ki predstavlja denarno enoto, katero bo pretvarjal drug razred. Razred ima le eno lastnost, ki označuje tip enote (npr. EUR, USD, …). Napiši še en razred, ki predstavlja pretvornik enot. Razred naj ima metodo, ki sprejme vrednost in enoto v kateri je vrednost ter instanco razreda enote, v katero naj vrednost pretvori. Metoda naj vrača tuple: pretvorjeno vrednost in tečaj, katerega je pretvornik uporabil. Metoda izgleda nekaj podobnega:
+*/
+
+import UIKit
+
+class Currency {
+    var currency: String
+    
+    init(currency: String) {
+        self.currency = currency
+    }
+}
+
+class ConverterBrain {
+
+    let exchangeRate = ["EUR2USD": 1.13585, "EUR2EUR": 1.0, "EUR2JPY": 136.044562]
+    
+    func convert(value: Double, startCurrency: String, targetCurrency: Currency) -> (convertedValue: Double, targetCurrency: String)? {
+        let a2b = startCurrency + "2" + targetCurrency.currency
+        let b2a = targetCurrency.currency + "2" + startCurrency
+        
+        if let rate = exchangeRate[a2b] {
+            return(value * rate, "Exchange Rate: " + a2b + "\(rate)")
+        }
+        else if let rate = exchangeRate[b2a] {
+            return(value * (1/rate), "Exchange Rate for " + b2a + " = " + "\(1/rate)")
+        }
+        else {
+            print("Not supported.")
+            return nil
+        }
+    }
+}
+
+//Test cases
+//let simpleConverter = ConverterBrain()
+
+//let eurInstance = Currency(currency:"EUR")
+//let usdInstance = Currency(currency:"USD")
+
+//let test = simpleConverter.convert(200.0, startCurrency:"USD", targetCurrency:eurInstance)
+//let test2 = simpleConverter.convert(200.0, startCurrency:"EUR", targetCurrency:usdInstance)
+
---
+++ Lesson3/Converter/Converter/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by amela on 19/10/15.
+//  Copyright © 2015 amela. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    let converter = ConverterBrain()
+    
+    let eurInstance = Currency(currency:"EUR")
+    let usdInstance = Currency(currency:"USD")
+    let jpyInstance = Currency(currency:"JPY")
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func convertButtons(sender: AnyObject) {
+        
+        if (sender as? UIButton != nil) {
+            
+            if let text = sender.currentTitle {
+                if let num = Double(textField.text!) {
+                    switch(text!) {
+                    case "USD":
+                        if let usd = converter.convert(num, startCurrency:"EUR", targetCurrency:usdInstance) {
+                            resultLabel.text = String(format: " = %.2f $", usd.0)
+                        }
+                    
+                    case "JPY":
+                        if let jpy = converter.convert(num, startCurrency:"EUR", targetCurrency:jpyInstance) {
+                            resultLabel.text = String(format: " = %.2f ¥", jpy.0)
+                        }
+                        
+                    case "EUR":
+                        if let eur = converter.convert(num, startCurrency:"EUR", targetCurrency:eurInstance) {
+                            resultLabel.text = String(format: " = %.2f €", eur.0)
+                        }
+                        
+                    default:
+                        resultLabel.text = "Fail"
+                    }
+                }
+                else {
+                    let alertView = UIAlertController(title: "Invalid number", message: "Please enter a number.", preferredStyle: .Alert)
+                    alertView.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                    presentViewController(alertView, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
