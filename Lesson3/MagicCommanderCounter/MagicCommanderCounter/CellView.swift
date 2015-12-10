7e97bfe908933a752db3b71b50302fb00f6573e4
amela <amela.rakanovic@gmail.com>
2015-12-10 09:28:49 +0100

    Update Magic Life.

---
+++ Lesson3/MagicCommanderCounter/MagicCommanderCounter/CellView.swift
@@ -0,0 +1,9 @@
+//
+//  CellView.swift
+//  MagicCommanderCounter
+//
+//  Created by amela on 10/12/15.
+//  Copyright © 2015 amela. All rights reserved.
+//
+
+import Foundation
---
+++ Lesson3/TestingPlayground/TestingPlayground/test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  TestingPlayground
+//
+//  Created by amela on 07/12/15.
+//  Copyright © 2015 amela. All rights reserved.
+//
+
+import Foundation
